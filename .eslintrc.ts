9ca92c264e7975358d9b083496854de0d576e69c
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-30 22:19:16 +0900

    [#1] Chore: Initial setting

---
+++ .eslintrc.ts
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {},
+};
