27974f4b9ab7a091d3dd02d7a6606a893d3c8608
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-31 14:11:01 +0900

    [#6] Chore: Set up absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            src: './src',
+            components: './src/components',
+            constants: './src/constants',
+            data: './src/data',
+            hooks: './src/hooks',
+            screens: './src/screens',
+            store: './src/store',
+            styles: './src/styles',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
