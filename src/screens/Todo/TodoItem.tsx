c7af427764c9cd8bea2e700f7a560dbaa3de464d
yisu-kim <yisu.kim.dev@outlook.com>
2021-09-01 20:22:10 +0900

    [#19] Feat: Dispatch toggleTodo & deleteTodo in TodoItem

--- src/screens/Todo/TodoItem.tsx
+++ src/screens/Todo/TodoItem.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { ITodo } from 'constants/todoTypes';
+import { useDispatch } from 'react-redux';
+import { deleteTodo, toggleTodo } from 'store/actions/todo';
+import { useState } from 'react';
+import TodoEditForm from './TodoEditForm';
 
 interface ITodoItemProps {
-  todo: number;
+  todo: ITodo;
 }
 
 const TodoItem: React.FC<ITodoItemProps> = ({ todo }) => {
-  return <Text>{todo}</Text>;
+  const [showEditForm, setShowEditForm] = useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  const handlePressCheckButton = () => {
+    dispatch(toggleTodo(todo.id, !todo.isCheck));
+  };
+
+  const toggleShowEditForm = () => {
+    setShowEditForm((prev) => !prev);
+  };
+
+  const handlePressDeleteButton = () => {
+    dispatch(deleteTodo(todo.id));
+  };
+
+  return (
+    <View style={styles.todoItem}>
+      <Button
+        title={todo.isCheck === true ? 'O' : 'V'}
+        onPress={handlePressCheckButton}
+      ></Button>
+      {showEditForm ? (
+        <TodoEditForm todo={todo} toggleShowEditForm={toggleShowEditForm} />
+      ) : (
+        <Text style={styles.content} onPress={toggleShowEditForm}>
+          {todo.content}
+        </Text>
+      )}
+      <Button title="X" onPress={handlePressDeleteButton}></Button>
+    </View>
+  );
 };
 
 export default TodoItem;
+
+const styles = StyleSheet.create({
+  todoItem: {
+    flexDirection: 'row',
+  },
+  content: {
+    flex: 1,
+  },
+});
