2d1ec779eba7b9375fa477a0298d4fb7b3c77277
yisu-kim <yisu.kim.dev@outlook.com>
2021-09-01 20:16:28 +0900

    [#19] Feat: Dispatch addTodo in TodoInputForm

--- src/screens/Todo/TodoInputForm.tsx
+++ src/screens/Todo/TodoInputForm.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
+import { ITodo } from 'constants/todoTypes';
+import { addTodo } from 'store/actions/todo';
+import { RootState } from 'store/reducers';
+
+type TodoContent = ITodo['content'];
 
 const TodoInputForm: React.FC = () => {
+  const [content, setContent] = useState<TodoContent>('');
+  const { nextId } = useSelector((state: RootState) => state.todo);
+  const dispatch = useDispatch();
+
+  const handleChange = (text: TodoContent) => {
+    setContent(text);
+  };
+
+  const handleSubmit = () => {
+    dispatch(addTodo(nextId, content));
+    setContent('');
+  };
+
   return (
     <View style={styles.inputForm}>
-      <Text>This is Input Form</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="할 일을 입력해주세요."
+        onChangeText={handleChange}
+        onSubmitEditing={handleSubmit}
+        defaultValue={content}
+      />
+      <Button title="+" onPress={handleSubmit}></Button>
     </View>
   );
 };
@@ -14,5 +40,9 @@ export default TodoInputForm;
 const styles = StyleSheet.create({
   inputForm: {
     backgroundColor: 'skyblue',
+  },
+  textInput: {
+    height: 40,
+    paddingHorizontal: 10,
   },
 });
